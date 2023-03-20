e3f4b101d104268b4e28f9bef04f78e59ffbde4b
Ryan Gordon <ryg@google.com>
2023-03-20 15:10:44 -0700

    ls logs

--- predict.py
+++ predict.py
@@ -144,6 +144,9 @@ class Predictor(BasePredictor):
                 output_path_strings.append(output_path)
         
         os.system(f"python3 inference_video.py --exp=4 --img=/tmp/imgs/ --output=/tmp/test.mp4")
+        os.system(f"ls .")
+        os.system(f"ls /tmp")
+        os.system(f"ls /tmp/imgs")
 
         # clips = [ImageClip(m).set_duration(0.1) for m in output_path_strings]
 
